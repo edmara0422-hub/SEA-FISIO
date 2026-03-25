d47ffd2d3b59bcedae400710aeae0681aa62be24
Edmara Rocha <edmara0422@gmail.com>
2026-03-25 13:56:57 -0300

    s

--- components/sea/top-bar-sea.tsx
+++ components/sea/top-bar-sea.tsx
@@ -9,7 +9,7 @@ export function TopBarSEA() {
 
   return (
     <motion.header
-      className="fixed left-0 right-0 top-0 z-40 px-2.5 pt-2.5 md:px-8"
+      className="fixed left-0 right-0 top-0 z-40 px-2.5 pt-5 md:px-8 md:pt-6"
       initial={{ y: -80, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.8, ease: [0.16, 1, 0.3, 1] }}
